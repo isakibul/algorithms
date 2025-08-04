ebf5eba92ded620352b2f1c6441790e5a7707229
isakibul <isakibul108@gmail.com>
2025-08-04 11:18:56 +0600

    add test

--- src/db/index.js
+++
@@ -1,5 +0,0 @@
-const connectDatabase = require("./connectDatabase");
-
-module.exports = {
-  connectDatabase,
-};
--- src/model/index.js
+++
@@ -1,7 +0,0 @@
-const User = require("./User");
-const Issue = require("./Issue");
-
-module.exports = {
-  User,
-  Issue,
-};
